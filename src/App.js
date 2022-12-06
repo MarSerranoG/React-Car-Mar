6c3e41a95abc62823f7021d5e2be1e5d7c56a8e9
MarSerranoG <89483003+MarSerranoG@users.noreply.github.com>
2022-12-06 10:42:15 -0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ export default function App() {
         nombre="Gorillaz Stylo Car"
         modelo="Camaro 69"
         imagen="https://s1.cdn.autoevolution.com/images/news/someone-has-recreated-the-chevrolet-camaro-from-gorillazs-stylo-for-gts-143187-7.jpg"
-        descripcion="El auto mas veloz de la serie de peliculas de Cars."
+        descripcion="El auto de la banda gorillaz."
         style={{ margin: '6px' }}
       />
       <Car
         nombre="Mustang Charge"
         modelo="1967"
         imagen="https://cdn.forbes.com.mx/2019/07/mustang-auto-carro-charge-electrico-verde-640x360.jpg"
-        descripcion="Restomod eléctrico del clásico de Ford de 1976 "
+        descripcion="Restomod eléctrico del clásico de Ford de 1976."
         style={{ margin: '6px' }}
       />
     </div>
